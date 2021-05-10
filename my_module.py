275b0a62abee57307fac4804fab21aea7f93904d
chsy0123 <chsy0123@naver.com>
2021-05-10 16:40:00 +0900

    initial commit

--- my_module.py
+++ my_module.py
@@ -1,11 +1,19 @@
-def add(a, b) :
-	return a+b;
+def add(a, b):
+    return a+b
 
-def sub(a, b) :
-	return a-b;
 
-def mul(a, b) :
-	return a*b;
+def sub(a, b):
+    return a-b
 
-def div(a, b) :
-	return a/b;
+
+def mul(a, b):
+    return a*b
+
+
+def div(a, b):
+    return a/b
+
+
+print("Hello World")
+print(add(1, 2))
+print(sub(4, 5))
